4948a270d65e2cefbdfdfdbbb6ef6ab70ff4d74a
miliarisgustavo <miliarisgustavo@gmail.com>
2024-07-10 22:19:09 +0100

    criação da tela de consumo de combustivel

--- app/src/main/java/com/example/fuelcalculator/ConsumptionActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/ConsumptionActivity.kt
@@ -9,20 +9,20 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.textfield.TextInputEditText
 
-class PriceActivity : AppCompatActivity() {
+class ConsumptionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_price)
+        setContentView(R.layout.activity_consumption)
 
-        val edtPrice = findViewById<TextInputEditText>(R.id.edt_Price)
+        val edtConsumption = findViewById<TextInputEditText>(R.id.edt_Consumption)
         val btnNext = findViewById<Button>(R.id.btn_Next)
         btnNext.setOnClickListener {
-            val priceStr: String = edtPrice.text.toString()
+            val consumptionStr: String = edtConsumption.text.toString()
             println("teste")
 
             val intent = Intent(this, ConsumptionActivity::class.java)
             startActivity(intent)
-        }
+
     }
 }
